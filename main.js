a78d5e51cea8c0b596c1334d8a7318deecefcbdb
Guilherme Vinicios Tramontin <87162210015@canoas.unilasalle.edu.br>
2022-10-18 15:33:46 -0300

    everything working

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function calcular() {
-    alert('Eu sou um alerta que ta funcionando')
+function pegarNum() {
+    var resp = document.getElementById('number').value
+    var respostF = 0; 
+    var res = document.getElementById('result')
+
+    if (! /^[01]+$/.test(resp)) {
+            return alert("Por favor, digite um número binário");
+        }
+    
+    respostF = parseInt(resp,2)
+    
+    res.innerHTML = respostF
 }
